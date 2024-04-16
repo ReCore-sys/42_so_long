167f190aa580adb7f502f36ef20564ecb373bf88
ReCore-sys <kjshore2004@gmail.com>
2024-04-16 20:08:58 +0930

    I am in much pain. It will not end.

---
+++ game_logic/graphics.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   graphics.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kshore <kshore@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/14 19:06:51 by kshore            #+#    #+#             */
+/*   Updated: 2024/04/16 19:52:47 by kshore           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../headers/so_long.h"
+
+void	place_player(t_complete *game, int height, int width)
+{
+	mlx_put_image_to_window(game->mlxpointer,
+		game->winpointer, game->player, width * 32, height * 32);
+	game->y_axis = height;
+	game->x_axis = width;
+}
+
+void	place_collectable(t_complete *game, int height, int width)
+{
+	mlx_put_image_to_window(game->mlxpointer,
+		game->winpointer, game->collectable, width * 32, height * 32);
+	game->collectables++;
+}
+
+void	place_images_in_game(t_complete *game)
+{
+	int	i;
+	int	j;
+
+	game->floor = mlx_xpm_file_to_image(game->mlxpointer,
+			"game_images/floor.xpm", &i, &j);
+	game->barrier = mlx_xpm_file_to_image(game->mlxpointer,
+			"game_images/wall.xpm", &i, &j);
+	game->player = mlx_xpm_file_to_image(game->mlxpointer,
+			"game_images/player.xpm", &i, &j);
+	game->exit = mlx_xpm_file_to_image(game->mlxpointer,
+			"game_images/exit.xpm", &i, &j);
+	game->collectable = mlx_xpm_file_to_image(game->mlxpointer,
+			"game_images/gold.xpm", &i, &j);
+}
+
+void	adding_in_graphics(t_complete *game)
+{
+	int	height;
+	int	width;
+
+	game->collectables = 0;
+	height = 0;
+	while (height < game->heightmap)
+	{
+		width = 0;
+		while (game->map[height][width])
+		{
+			if (game->map[height][width] == '1')
+				mlx_put_image_to_window(game->mlxpointer,
+					game->winpointer, game->barrier, width * 32, height * 32);
+			if (game->map[height][width] == 'C')
+				place_collectable(game, height, width);
+			if (game->map[height][width] == 'P')
+				place_player(game, height, width);
+			if (game->map[height][width] == 'E')
+				mlx_put_image_to_window(game->mlxpointer,
+					game->winpointer, game->exit, width * 32, height * 32);
+			if (game->map[height][width] == '0')
+				mlx_put_image_to_window(game->mlxpointer,
+					game->winpointer, game->floor, width * 32, height * 32);
+			width++;
+		}
+		height++;
+	}
+}
---
+++ game_logic/so_long.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kshore <kshore@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/14 16:49:02 by kshore            #+#    #+#             */
+/*   Updated: 2024/04/16 19:50:07 by kshore           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../headers/so_long.h"
+
+static void	*ft_memset(void *b, int c, size_t length)
+{
+	unsigned char	*p;
+
+	p = (unsigned char *)b;
+	while (length--)
+		*p++ = (unsigned char)c;
+	return (b);
+}
+
+int	exit_point(t_complete *game)
+{
+	int	line;
+
+	line = 0;
+	if (game->winpointer)
+		mlx_destroy_window(game->mlxpointer, game->winpointer);
+	free(game->mlxpointer);
+	while (line < game->heightmap - 1)
+		free(game->map[line++]);
+	free(game->map);
+	exit(0);
+}
+
+int	main(int argc, char **argv)
+{
+	t_complete	game;
+
+	if (argc != 2)
+		return (0);
+	ft_memset(&game, 0, sizeof(t_complete));
+	map_reading(&game, argv);
+	check_errors(&game);
+	game.mlxpointer = mlx_init();
+	game.winpointer = mlx_new_window(game.mlxpointer, (game.widthmap * 32),
+			(game.heightmap * 32), "solong");
+	place_images_in_game(&game);
+	adding_in_graphics(&game);
+	mlx_key_hook(game.winpointer, controls_working, &game);
+	mlx_hook(game.winpointer, 17, 0, (void *)exit, 0);
+	mlx_loop(game.mlxpointer);
+}
